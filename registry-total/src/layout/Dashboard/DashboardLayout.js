b69a09e162db050151be7759c4651d972d4b0786
My <my@example.com>
2023-06-10 21:45:12 +0700

    add bar

--- registry-total/src/layout/Dashboard/DashboardLayout.js
+++ registry-total/src/layout/Dashboard/DashboardLayout.js
@@ -20,8 +20,21 @@ import "./DashboardLayout.css";
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 import Select from "react-select";
+import { useSelector } from "react-redux";
+import Stack from "@mui/material/Stack";
+
+const customStyles = {
+
+    control: (defaultStyles) => ({
+        ...defaultStyles,
+        width: '95%',
+        marginLeft: '10px'
+    }),
+    singleValue: (defaultStyles) => ({ ...defaultStyles, color: "black" }),
+  };
 
 export default function DashboardLayout() {
+    const user = useSelector((state) => state.auth.user);
     const [registered_cars, setRegisteredCars] = useState([]);
     const [year_registered_cars, setYearRegisteredCars] = useState([]);
     const [expiring_cars, setExpiringCars] = useState([]);
@@ -136,7 +149,7 @@ export default function DashboardLayout() {
     }
 
     useEffect(() => {
-          window.addEventListener('error', e => {
+            window.addEventListener('error', e => {
             if (e.message === 'ResizeObserver loop limit exceeded') {
                 const resizeObserverErrDiv = document.getElementById(
                     'webpack-dev-server-client-overlay-div'
@@ -233,12 +246,13 @@ export default function DashboardLayout() {
             </div>
             }
             <div className="block-content-container">
-                <h4 className="chart-title">Số lượng xe ô tô đã đăng kiểm trong năm 2023</h4>
+                <h4 className="chart-title">Số lượng xe ô tô đã đăng kiểm trong năm</h4>
                 <div style={{display: 'flex'}}>
                   <div className="statistics-line-chart">
                     <div style={{overflowX: 'scroll', paddingBottom: '16px'}}>
-                        <Select options={years} onChange={handleChange} />
-                        <ResponsiveContainer width={1200} height={400}>
+                        <Select options={years} onChange={handleChange} styles={customStyles}/>
+                        <br></br>
+                        <ResponsiveContainer width={600} height={400}>
                           <AreaChart
                             data={selectdata()}
                             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
@@ -275,70 +289,120 @@ export default function DashboardLayout() {
                           />
                           </AreaChart>
                       </ResponsiveContainer>
-                      <div className="statistics-bar">
-                            <span className="statis"> Thống kê </span>
-                            <hr className="space"></hr>
-                            <ul className="progress-bar">
-                                <li>
-                                    <p className="quarter">
-                                        Quý 1
-                                    </p>
-                                    <Box sx={{ width: '20%' }}>
-                                        <LinearProgress sx={{backgroundColor: 'rgb(237, 237, 237)', 
-                                                            '& .MuiLinearProgress-bar': {
-                                                                backgroundColor: 'rgb(236, 64, 122)'
-                                                            }}} 
-                                                        variant="determinate" value={quarter[0].Total} />
-                                    </Box>
-                                </li>
-                                <li>
-                                    <p className="quarter">
-                                        Quý 2
-                                    </p>
-                                    <Box sx={{ width: '20%' }}>
-                                        <LinearProgress sx={{backgroundColor: 'rgb(237, 237, 237)', 
-                                                            '& .MuiLinearProgress-bar': {
-                                                                backgroundColor: 'rgb(251, 140, 0)'
-                                                            }}} 
-                                                        variant="determinate" value={quarter[1].Total} />
-                                    </Box>
-                                </li>
-                                <li>
-                                    <p className="quarter">
-                                        Quý 3
-                                    </p>
-                                    <Box sx={{ width: '20%' }}>
-                                        <LinearProgress sx={{backgroundColor: 'rgb(237, 237, 237)', 
-                                                            '& .MuiLinearProgress-bar': {
-                                                                backgroundColor: 'rgb(124, 179, 66)'
-                                                            }}} 
-                                                        variant="determinate" value={quarter[2].Total} />
-                                    </Box>
-                                </li>
-                                <li>
-                                    <p className="quarter">
-                                        Quý 4
-                                    </p>
-                                    <Box sx={{ width: '20%' }}>
-                                        <LinearProgress sx={{backgroundColor: 'rgb(237, 237, 237)', 
-                                                            '& .MuiLinearProgress-bar': {
-                                                                backgroundColor: 'rgb(3, 169, 244)'
-                                                            }}} 
-                                                        variant="determinate" value={quarter[3].Total} />
-                                    </Box>
-                                </li>
-                            </ul>
-                    </div>
                       </div>
                     </div>
                     <div className="quater-container">
-                      <p>Quý</p>
+                        <span className="statis"> Thống kê </span>
+                        <hr className="space"></hr>
+                        <ul className="progress-bar">
+                            <li>
+                                <div style = {{width:"100%"}}>
+                                    <p className="quarter">
+                                        Quý 1
+                                    </p>
+                                    <Box sx={{ width: '100%' }}>
+                                        <LinearProgress sx={{backgroundColor: 'rgb(237, 237, 237)', 
+                                                        '& .MuiLinearProgress-bar': {
+                                                            backgroundColor: 'rgb(236, 64, 122)'
+                                                        }}} 
+                                                        variant="determinate" value={quarter[0].Total} />
+                                    </Box>
+                                </div>
+                            </li>
+                            <li>
+                                <div style = {{width:"100%"}}>
+                                    <p className="quarter">
+                                        Quý 2
+                                    </p>
+                                    <Box sx={{ width: '100%' }}>
+                                        <LinearProgress sx={{backgroundColor: 'rgb(237, 237, 237)', 
+                                                        '& .MuiLinearProgress-bar': {
+                                                            backgroundColor: 'rgb(251, 140, 0)'
+                                                        }}} 
+                                                        variant="determinate" value={quarter[1].Total} />
+                                    </Box>
+                                </div>
+                            </li>
+                            <li>
+                                <div style = {{width:"100%"}}>
+                                    <p className="quarter">
+                                        Quý 3
+                                    </p>
+                                    <Box sx={{ width: '100%' }}>
+                                        <LinearProgress sx={{backgroundColor: 'rgb(237, 237, 237)', 
+                                                        '& .MuiLinearProgress-bar': {
+                                                            backgroundColor: 'rgb(124, 179, 66)'
+                                                        }}} 
+                                                        variant="determinate" value={quarter[2].Total} />
+                                    </Box>
+                                </div>
+                            </li>
+                            <li>
+                                <div style = {{width:"100%"}}>
+                                    <p className="quarter">
+                                        Quý 4
+                                    </p>
+                                    <Box sx={{ width: '100%' }}>
+                                        <LinearProgress sx={{backgroundColor: 'rgb(237, 237, 237)', 
+                                                        '& .MuiLinearProgress-bar': {
+                                                            backgroundColor: 'rgb(3, 169, 244)'
+                                                        }}} 
+                                                        variant="determinate" value={quarter[3].Total*100} />
+                                    </Box>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
-                  </div>
+                </div>
             </div>
-            {/* <div className="statistics-line-chart">
-                
-            </div> */}
+            <div className="block-content-container">
+                <h4 className="chart-title">Số lượng xe ô tô sắp hết hạn đăng kiểm trong năm</h4>
+                <div style={{display: 'flex'}}>
+                  <div className="statistics-line-chart">
+                    <div style={{overflowX: 'scroll', paddingBottom: '16px'}}>
+                        <Select options={years} onChange={handleChange} styles={customStyles}/>
+                        <br></br>
+                        <ResponsiveContainer width='100%' height={400}>
+                          <AreaChart
+                            data={selectdata()}
+                            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+                          >
+                          <defs>
+                              <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+                              <stop offset="5%" stopColor="rgb(23, 193, 232)" stopOpacity={0.2} />
+                              <stop offset="70%" stopColor="rgb(23, 193, 232)" stopOpacity={0.1} />
+                              </linearGradient>
+                              <linearGradient id="missing" x1="0" y1="0" x2="0" y2="1">
+                              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.4} />
+                              <stop offset="70%" stopColor="#8884d8" stopOpacity={0.2} />
+                              </linearGradient>
+                          </defs>
+                          <XAxis dataKey="name" stroke="gray" />
+                          <YAxis/>
+                          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+                          <Tooltip />
+                          <Area
+                              type="monotone"
+                              dataKey="Total"
+                              stroke="rgb(23, 193, 232)"
+                              fillOpacity={1}
+                              strokeWidth={4}
+                              fill="url(#total)"
+                          />
+                          <Area
+                              type="monotone"
+                              dataKey="Missing"
+                              stroke="#8884d8"
+                              fillOpacity={1}
+                              strokeWidth={4}
+                              fill="url(#missing)"
+                          />
+                          </AreaChart>
+                      </ResponsiveContainer>
+                      </div>
+                    </div>
+                </div>
+            </div>
         </div>
   )
 }
